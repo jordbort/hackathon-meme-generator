023865f5a5cbb3cf1854d8e42c429bc357ce5c63
jordbort <jordan.bortner@gmail.com>
2023-01-12 18:54:04 -0500

    making header a link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header(props) {
     return (
         <>
-        <img src="https://cdn6.aptoide.com/imgs/9/9/f/99f4b62f7dc2384bba8fbc97e82ec31f_feature_graphic.jpg" />
+        <a href="./create"><img src="https://cdn6.aptoide.com/imgs/9/9/f/99f4b62f7dc2384bba8fbc97e82ec31f_feature_graphic.jpg" /></a>
         </>
     )
 }
