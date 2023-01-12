eca74e59c64e5487b7a57f6d516c0a8c12a1bebb
jordbort <jordan.bortner@gmail.com>
2023-01-12 17:11:05 -0500

    Get meme image form working

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,18 +1,31 @@
 import { useState, useEffect } from 'react'
-// import { Link } from 'react-router-dom'
 
 function CreatePost(props) {
     const [memes, setMemes] = useState([])
+    const [selectedMeme, setSelectedMeme] = useState(null)
+    // const [form, setForm] = useState()
 
     const getMemes = async () => {
         try {
             const res = await fetch('https://api.imgflip.com/get_memes')
             const info = await res.json()
-            console.log(info.data)
+            // console.log(info.data)
             setMemes(info.data.memes)
         } catch (error) {
             console.log(error)
         }
+    }
+
+    // async function handleSubmit(event) {
+    //     event.preventDefault()
+    //     console.log(selectedMeme)
+    //     setSelectedMeme(event.target.value)
+    // }
+    // console.log(selectedMeme)
+
+    function handleChange(event) {
+        // console.log(event.target.value)
+        setSelectedMeme(event.target.value)
     }
 
     useEffect(() => {
@@ -21,19 +34,29 @@ function CreatePost(props) {
 
     const loaded = () => {
         return (
-            <h2>Create Meme</h2>            // memes.map((meme, idx) => (
-            //     <div key={idx}>
-            //         <Link to={`/create`}><img src={meme.url} /></Link>
-            //         <h3> {meme.name}</h3>
-            //     </div>
-            // ))
+            <>
+                <h2>Create Meme</h2>
+                {/* <form onSubmit={handleSubmit}> */}
+                    {/* <label htmlFor='memes'>Select meme format: </label> */}
+                    <select name='memes' onChange={handleChange}>
+                        {memes.map((meme, idx) => (
+                            <option key={idx} value={meme.url}>{meme.name}</option>
+                        ))}
+                    </select>
+                    {/* <input type='submit' value='Select meme' /> */}
+                {/* </form> */}
+                <h1>Create meme</h1>
+                <div>
+                    <img src={selectedMeme} alt="" />
+                </div>
+            </>
         )
     }
 
     const loading = () => {
-        <div>Loading. . .</div>
+        return <h1>Loading. . .</h1>
     }
-    return memes ? loaded() : loading()
+    return memes && memes.length ? loaded() : loading()
 }
 
 export default CreatePost
