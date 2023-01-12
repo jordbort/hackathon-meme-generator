3dd63cbb4ed537a35063b3bdebbbf3ec062bf9e1
jordbort <jordan.bortner@gmail.com>
2023-01-12 12:54:18 -0500

    Adding dummy pages

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,7 @@
+function SearchBar(props) {
+    return (
+        <h2>SearchBar</h2>
+    )
+}
+
+export default SearchBar
---
+++ src/pages/UserLogin.jsx
@@ -0,0 +1,7 @@
+function UserLogin(props) {
+    return (
+        <h1>UserLogin</h1>
+    )
+}
+
+export default UserLogin
