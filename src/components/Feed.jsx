03150433351bb5f954435f424b093f9d543b8062
ShadowTorn33 <austincheer94@gmail.com>
2023-01-12 11:20:54 -0800

    fetch funciton

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,21 @@
 function Feed(props) {
+
+    const [memes, setMemes] = useState([])
+
+    const getMemes = async () => {
+        try {
+            const res = await fetch('https://api.imgflip.com/get_memes')
+            const data = await res.json()
+            console.log(data)
+
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(() => {
+        getMemes()
+    }, [])
     return (
         <h2>Feed</h2>
     )
