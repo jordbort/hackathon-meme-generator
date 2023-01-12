1a59f54e7fa85edc9e1b054b7a1f7ba3ae4e6ddc
jordbort <jordan.bortner@gmail.com>
2023-01-12 14:47:24 -0500

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import CreatePost from './pages/CreatePost'
+import ProfileDetail from './pages/ProfileDetail'
+import UserLogin from './pages/UserLogin'
+import UserRegister from './pages/UserRegister'
+
 function App() {
     return (
         <>
-            <h1>Meme Generator</h1>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/create' element={<CreatePost />} />
+                <Route path='/profile/:id' element={<ProfileDetail />} />
+                <Route path='/login' element={<UserLogin />} />
+                <Route path='/register' element={<UserRegister />} />
+            </Routes>
         </>
     )
 }
