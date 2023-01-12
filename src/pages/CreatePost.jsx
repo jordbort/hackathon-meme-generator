4ba6339f46160ec35e8de09847c10f826a85cd48
ShadowTorn33 <austincheer94@gmail.com>
2023-01-12 12:11:04 -0800

    Moved feed code to createPost

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
 function CreatePost(props) {
-    return (
-        <h1>CreatePost</h1>
-    )
+    const [memes, setMemes] = useState([])
+
+    const getMemes = async () => {
+        try {
+            const res = await fetch('https://api.imgflip.com/get_memes')
+            const info = await res.json()
+            console.log(info.data)
+            setMemes(info.data.memes)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getMemes()
+    }, [])
+
+    const loaded = () => {
+        return memes.map((meme, idx) => (
+            <div>
+                <Link to={`/create`}><img src={meme.url} /></Link>
+                <h3> {meme.name}</h3>
+            </div>
+        ))
+    }
+
+    const loading = () => {
+        <div>Loading. . .</div>
+    }
+    return memes ? loaded() : loading()
 }
 
 export default CreatePost
