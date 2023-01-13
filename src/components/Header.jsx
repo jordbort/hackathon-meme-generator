d4490040e979019086b94d32fd0a17576957a0ef
jordbort <jordan.bortner@gmail.com>
2023-01-12 20:43:25 -0500

    Adding links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 function Header(props) {
     return (
         <>
-        <a href="./create"><img src="https://cdn6.aptoide.com/imgs/9/9/f/99f4b62f7dc2384bba8fbc97e82ec31f_feature_graphic.jpg" /></a>
+            <Link to="/"><img src="https://cdn6.aptoide.com/imgs/9/9/f/99f4b62f7dc2384bba8fbc97e82ec31f_feature_graphic.jpg" /></Link>
+            <br />
+            <Link to="/create">Create a Meme</Link>
         </>
     )
 }
