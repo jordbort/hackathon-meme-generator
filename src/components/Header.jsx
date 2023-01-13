4017058ad8771faa40c13e1340544f409fecc699
jordbort <jordan.bortner@gmail.com>
2023-01-12 20:55:26 -0500

    Applied header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 
 function Header(props) {
     return (
-        <>
+        <header>
             <Link to="/"><img src="https://cdn6.aptoide.com/imgs/9/9/f/99f4b62f7dc2384bba8fbc97e82ec31f_feature_graphic.jpg" /></Link>
             <br />
             <Link to="/create">Create a Meme</Link>
-        </>
+        </header>
     )
 }
 
